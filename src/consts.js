13a2c20883e1fef1e530acca04aff10c845bf1ab
Cristian Valdez <cvaldezana@gmail.com>
2026-02-12 14:49:50 +0900

    fix language accessiblity and seo stuff i guess

--- src/consts.js
+++ src/consts.js
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Bloggy Doggy";
-export const SITE_DESCRIPTION = "Welcome to my blog.";
+export const SITE_DESCRIPTION = "Life in the Japanese countryside—stories, photos, and observations in easy-to-understand English.";
