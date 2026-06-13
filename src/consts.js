6fb84147b60bfd05a5cb35ae076e91379bb1b65c
the dawg pc <my email>
2026-06-13 14:40:21 +0900

    SEO fixes, theme color consitancy update, add lastmod to sitemap

--- src/consts.js
+++ src/consts.js
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Bloggy Doggy";
-export const SITE_DESCRIPTION = "Life in the Japanese countryside—stories, photos, and observations in easy-to-understand English.";
+export const SITE_DESCRIPTION = "Life in the Japanese countryside: stories, photos, and observations in easy-to-understand English.";
+export const SITE_AUTHOR = "Cristian";
